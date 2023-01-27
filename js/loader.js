e6e4d66b432332c20ec75a0a98a051af7bc3df58
Amit Patel <77044688+amitpatel8602@users.noreply.github.com>
2023-01-27 13:36:49 +0000

    updated hero banner

--- js/loader.js
+++ js/loader.js
@@ -1,4 +1,21 @@
 window.onload = function () {
-  document.getElementById("loader").style.display = "none";
+  // List of image URLs
+  var images = [
+    "images/11.jpg",
+    "images/12.jpg",
+    "images/13.jpg",
+    "images/14.jpg",
+    "images/15.jpg",
+    "images/16.jpg",
+    "images/17.jpg",
+    "images/18.jpg",
+    "images/19.jpg",
+    "images/20.jpg",
+  ];
+
+  // Get a random image URL
+  var randomImage = images[Math.floor(Math.random() * images.length)];
+
+  // Update the src attribute of the image element
+  document.getElementById("main-img").src = randomImage;
 };
-document.getElementById("loader").style.display = "block";
--- js/main.js
+++ js/main.js
@@ -1,21 +1,28 @@
 $.getJSON("json/jobopen.json", function (data) {
   var listings = data;
   var jobListings = $("#job-listings");
-  for (var i = 0; i < listings.length; i++) {
-    var listing = listings[i];
-    var jobListing = $(".job-listing").eq(i);
-    jobListing.append('<div class="job-listing">');
-    jobListing.append("<h3>" + listing.title + "</h3>");
-    jobListing.append("<p>Company: " + listing.companyname + "</p>");
-    jobListing.append("<p>Category: " + listing.category + "</p>");
-    jobListing.append("<p>Description: " + listing.description + "</p>");
-    jobListing.append("<p>Location: " + listing.location + "</p>");
+  if (listings.length > 0) {
+    for (var i = 0; i < listings.length; i++) {
+      var listing = listings[i];
+      var jobListing = $(".job-listing").eq(i);
+      jobListing.append('<div class="job-listing">');
+      jobListing.append("<h3>" + listing.title + "</h3>");
+      jobListing.append("<p>Company: " + listing.companyname + "</p>");
+      jobListing.append("<p>Category: " + listing.category + "</p>");
+      jobListing.append("<p>Description: " + listing.description + "</p>");
+      jobListing.append("<p>Location: " + listing.location + "</p>");
+      jobListing.append(
+        '<a href="' +
+          listing.apply +
+          '"' +
+          ' target="_blank" class="apply-button">Apply</a>'
+      );
+      jobListing.append("</div>");
+    }
+  } else {
+    var jobListing = $(".job-listing").eq(0);
     jobListing.append(
-      '<a href="' +
-        listing.apply +
-        '"' +
-        ' target="_blank" class="apply-button">Apply</a>'
+      '<h3 class="no-opening">' + "There is no openings currently..." + "</h3>"
     );
-    jobListing.append("</div>");
   }
 });
