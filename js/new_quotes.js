1586c59fc8dec1e022a3768197f03f39e788d487
Amit Patel <amipatel4@publicisgroupe.net>
2024-07-17 12:27:08 +0530

    initial commit for new quote API

--- js/new_quotes.js
+++ js/new_quotes.js
@@ -1,5 +1,6 @@
-fetch(QUOTE_API_PATH)
+fetch(ZEN_QUOTE_API_PATH)
   .then((response) => {
+    console.log(response);
     if (response.ok) {
       return response.json();
     } else {
@@ -7,8 +8,9 @@ fetch(QUOTE_API_PATH)
     }
   })
   .then((data) => {
+    console.log(data);
     var quote = document.getElementById("main-para");
-    var putData = data.content + "    \n-" + data.author;
+    var putData = data.q + "    \n-" + data.a;
     quote.innerText = putData;
   })
   .catch((error) => {
