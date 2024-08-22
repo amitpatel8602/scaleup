b2c90fca7e3d262596872b8bbe073a2cc66e7850
Amit Patel <amipatel4@publicisgroupe.net>
2024-08-22 11:57:17 +0530

    adding logic to include days in experience

--- js/experience.js
+++ js/experience.js
@@ -4,6 +4,13 @@ setTimeout(() => {
   var d2 = new Date(dPast);
   var years = d1.getFullYear() - d2.getFullYear();
   var months = d1.getMonth() - d2.getMonth();
+  var dayDifference = d1.getDate() - d2.getDate();
+  if (dayDifference < 0) {
+    months--;
+    // Get the previous month
+    let lastMonth = new Date(d1.getFullYear(), d1.getMonth() - 1, d2.getDate());
+    dayDifference += new Date(lastMonth.getFullYear(), lastMonth.getMonth() + 1, 0).getDate();
+}
   if (months < 0) {
     years--;
     months += 12;
@@ -11,9 +18,13 @@ setTimeout(() => {
   if (months == 0) {
     document.getElementById("exp").innerText =
       "Total Experience - " + years + " years ";
-  } else {
+  } else if(dayDifference == 0){
     document.getElementById("exp").innerText =
-      "Total Experience - " + years + " years " + months + " months";
+    "Total Experience - " + years + " years " + months + " months";
+  }
+  else {
+    document.getElementById("exp").innerText =
+      "Total Experience - " + years + " years " + months + " months "+ dayDifference + " days";
   }
 }, 1000);
 document.getElementById("exp").innerText = "Total Experience - Calculating...";
