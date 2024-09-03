be9b668358d028dde64fd0dc3c4746f5a0045385
Amit Patel <amipatel4@publicisgroupe.net>
2024-09-03 16:23:59 +0530

    adding education in experience page

--- js/experiencedata.js
+++ js/experiencedata.js
@@ -2,14 +2,21 @@ $.getJSON(EXPERIENCE_BIN, function (data) {
   var yearwisedata = data;
   var timeline = $(".timeline").eq(0);
   var skillset = $(".skillset").eq(0);
+  var education = $(".education").eq(0);
   for (let index = 0; index < yearwisedata.length; index++) {
     var year = yearwisedata[index].year;
     var yearData = yearwisedata[index].yearData;
-    if (year !== "skill-data") {
-      timeline.append(
-        `<button class="year" onclick="showDetails(this, '${year}', '${yearData}')">${year}</button>`
+    console.log(year,yearData,typeof(year));
+    if(year.toString().includes('edu-')){
+      var year = year.split("-")[1];
+      education.append(
+        `<button class="year" onclick="showDetails(this, 'details-edu', '${year}', '${yearData}')">${year}</button>`
       );
-    } else {
+    }else if (year !== "skill-data") {
+      timeline.append(
+        `<button class="year" onclick="showDetails(this, 'details-time', '${year}', '${yearData}')">${year}</button>`
+      );
+    }else {
       var arrayset = [
         "primary",
         "secondary",
@@ -35,7 +42,7 @@ $.getJSON(EXPERIENCE_BIN, function (data) {
   }
 });
 
-function showDetails(button, year, yearData) {
+function showDetails(button, id, year, yearData) {
   // Remove 'active' class from any previously active button
   const activeButton = document.querySelector(".year.active");
   if (activeButton) {
@@ -46,6 +53,6 @@ function showDetails(button, year, yearData) {
   button.classList.add("active");
 
   // Update details
-  const detailsDiv = document.getElementById("details");
+  const detailsDiv = document.getElementById(id);
   detailsDiv.innerHTML = `<h4>${year}</h4><p>${yearData}</p>`;
 }
